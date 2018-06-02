322f3d12aa6fcfa958d2bc2019e7ed204285ce03
kusumaputra0647 <kusumaputra0647@gmail.com>
2018-06-02 11:03:15 +0800

    my commit

---
+++ App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.459556,
+      longitude: 115.046599,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.539153,
+          longitude: 115.132210
+        },
+        title: 'Rumah Sakit Tabanan'
+        subtitle: 'RSUD'
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.557628,
+          longitude: 115.141116
+        },
+        title: 'Kominfo Tabanan',
+        subtitle: 'Dinas Komunikasi Dan Informatika Tabanan'
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.537603,
+          longitude: 115.136054
+        },
+        title: 'Dangin Carik'
+      }
+      ,
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Peta Tabanan
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> Kusuma_idiot </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
